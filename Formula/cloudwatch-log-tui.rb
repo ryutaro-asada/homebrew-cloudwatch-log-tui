5e1e098aff9f0a2d214a5ee9528d024dff13964a
ryutaro-asada <deneimonmaru@gmail.com>
2025-06-16 21:17:23 +0900

    bump v0.1.1

--- Formula/cloudwatch-log-tui.rb
+++ Formula/cloudwatch-log-tui.rb
@@ -1,21 +1,21 @@
 class CloudwatchLogTui < Formula
   desc "Terminal UI for AWS CloudWatch Logs"
   homepage "https://github.com/ryutaro-asada/cloudwatch-log-tui"
-  version "0.1.0"
+  version "0.1.1"
   
   on_macos do
     if Hardware::CPU.intel?
       url "https://github.com/ryutaro-asada/cloudwatch-log-tui/releases/download/v#{version}/cloudwatch-log-tui-#{version}-darwin-amd64.tar.gz"
-      sha256 "1c25075509a50d2fa40ae089ea405bf4d9d0dd9501e09e4554feb25c0d8309c6" # 実際のハッシュ値に置き換える
+      sha256 "972e5ba5e30fe09d3224d338fcba7792bbe587a9545cf8eb269d60433942909a" # 実際のハッシュ値に置き換える
     else
       url "https://github.com/ryutaro-asada/cloudwatch-log-tui/releases/download/v#{version}/cloudwatch-log-tui-#{version}-darwin-arm64.tar.gz"
-      sha256 "0c6d56e53af46a05d8041e362148f4cbcbbfc212e93b2293e7a0270452b8c379" # 実際のハッシュ値に置き換える
+      sha256 "f999bff89be8060a0d265d6a9b44da2bcaa75a2df9cad80a31841e5ee1790419" # 実際のハッシュ値に置き換える
     end
   end
   
   on_linux do
     url "https://github.com/ryutaro-asada/cloudwatch-log-tui/releases/download/v#{version}/cloudwatch-log-tui-#{version}-linux-amd64.tar.gz"
-    sha256 "9b4bc52dc77ebb13cb90d3746e218ed15921599e2036a9a370d679d048564ce3" # 実際のハッシュ値に置き換える
+    sha256 "ff3fe5c5d1cbc6d99eca85696c9420defede98962582bacaaf8305c737867932" # 実際のハッシュ値に置き換える
   end
 
   def install
